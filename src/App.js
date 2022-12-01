586e987fe9c6f86efa56a10411f144dd6e545a88
“Victory <Victory23tshimbadi@gmail.com>
2022-12-01 12:34:48 +0100

    add link

--- src/App.js
+++ src/App.js
@@ -1,11 +1,7 @@
-import CustomerFlow from "./conponents/CustomerFlow";
-
+import BriefFlow from "./components/BriefFlow";
+import CustomFlow from "./components/CustomFlow";
 function App() {
-  return (
-    <div>
-      <CustomerFlow/>
-    </div>
-  );
+  return window.location.pathname === "/brief" ? <BriefFlow /> : <CustomFlow />;
 }
 
 export default App;
